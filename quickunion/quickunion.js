ebf8806f90763fe980a0aa897cc1d2df7d9bd270
Hunter Rafuse <hunter.rafuse22@gmail.com>
2020-01-27 13:30:07 +0200

    base push

--- quickunion/quickunion.js
+++ quickunion/quickunion.js
@@ -17,21 +17,24 @@ function QuickUnion(n){
     }
 
     this.connected= function(p, q) {
-        return this.id(this.p) === this.id(this.q);
+        return this.id[p] === this.id[q];
     }
 
 
-    this.union = function(i, j){
+    this.union = function(p,q){
         if (this.connected(p, q)) return;
         var pid = this.id[p];
-        for (var i = 0; i < this.id.length; i++) {
-            this.id[i] == p[id] ? this.[id] = this.id[q] : this.count--;
-        }
-       this.constructor(n);
+        for (var i = 0; i < this.id.length; i++)
+            if (this.id[i] == pid) this.id[i] = this.id[q];
+        this.count--;
+        
+     
+
     }
+    this.constructor(n);
 }
 
-var x = new QuickUnion(n);
+var x = new QuickUnion(10);
 console.log(x.id);
 
 var a = '2-1 3-7 6-9 3-4 6-7';
@@ -40,7 +43,7 @@ a = a.split(' ');
 
 for (let b = 0; b < a.length; b++) {
     var p = a[b].split('-')[0] | 0;
-    var q = a[j].split('-')[1] |0;
+    var q = a[b].split('-')[1] |0;
     x.union(p,q);
 }
 
